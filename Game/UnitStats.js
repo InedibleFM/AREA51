f0fd4062f38df2bb8d39cc147768aac97e85814d
RobertB01 <35612511+RobertB01@users.noreply.github.com>
2019-08-18 15:31:51 +0200

    Update UnitStats.js

--- Game/UnitStats.js
+++ Game/UnitStats.js
@@ -14,13 +14,13 @@ let unit1_1 = {
 
 let unit1_2 = {
 	maxHealth: 75,
-	damage: 15,
+	damage: 75,
     width: 42,
     height: 70,
     coinReward: 20,
     expReward: 100,
     attackingDelay: 80,
-    hasRange: 100,
+    hasRange: 500,
     restingAnimationSpeed: 0.08,
 	movingAnimationSpeed: 0.12
 }
